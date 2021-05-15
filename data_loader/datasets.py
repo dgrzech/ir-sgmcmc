data_loader/datasets.py: Fix initialisation of the low-rank covariance matrix
data_loader/datasets.py: Fix initialisation of the low-rank covariance matrix
model/loss.py: Fix initialisation of the low-rank covariance matrix
trainer/trainer.py: Fix initialisation of the low-rank covariance matrix

utils/sampler.py: Fix initialisation of the low-rank covariance matrix

parse_config.py: Fix initialisation of the low-rank covariance matrix
logger/visualization.py: Fix initialisation of the low-rank covariance matrix

config.json: Fix initialisation of the low-rank covariance matrix
config_baseline.json: Fix initialisation of the low-rank covariance matrix

data_loader/datasets.py
@@ -59,13 +59,13 @@ class BiobankDataset(Dataset):
 
     def _init_log_var_v(self):
         sigma_v = self.sigma_v_init
-        var_v = (sigma_v ** 2) * torch.ones(self.dims_v)
+        var_v = (sigma_v ** 2) + torch.zeros(self.dims_v)
 
-        return torch.log(var_v)
+        return var_v.log()
 
-    def _init_u_v(self):
-        u_v = self.u_v_init * torch.ones(self.dims_v)
-        return u_v
+    def _init_log_u_v(self):
+        u_v = self.u_v_init + torch.zeros(self.dims_v)
+        return u_v.log()
 
     def _get_image(self, im_path):
         im = sitk.ReadImage(im_path, sitk.sitkFloat32)
@@ -130,8 +130,8 @@ class BiobankDataset(Dataset):
         # q_v
         mu_v = self._init_mu_v()
         log_var_v = self._init_log_var_v()
-        u_v = self._init_u_v()
+        log_u_v = self._init_log_u_v()
 
-        var_params_q_v = {'mu': mu_v, 'log_var': log_var_v, 'u': u_v}
+        var_params_q_v = {'mu': mu_v, 'log_var': log_var_v, 'log_u': log_u_v}
 
         return fixed, moving, var_params_q_v

model/loss.py
@@ -350,18 +350,21 @@ class EntropyMultivariateNormal(Entropy):
     def forward(self, **kwargs):
         if len(kwargs) == 2:
             log_var = kwargs['log_var']
-            u = kwargs['u']
+            log_u = kwargs['log_u']
 
             sigma = torch.exp(0.5 * log_var)
+            u = torch.exp(log_u)
+
             return 0.5 * (torch.log1p(torch.sum(torch.pow(u / sigma, 2), dim=(1, 2, 3, 4))) + torch.sum(log_var, dim=(1, 2, 3, 4)))
         elif len(kwargs) == 4:
             sample = kwargs['sample']
 
             mu = kwargs['mu']
             log_var = kwargs['log_var']
-            u = kwargs['u']
+            log_u = kwargs['log_u']
 
             sigma = torch.exp(0.5 * log_var)
+            u = torch.exp(log_u)
 
             sample_n = (sample - mu) / sigma
             u_n = u / sigma

parse_config.py
@@ -190,7 +190,7 @@ class ConfigParser:
 
         # other
         other = ['VI/train/reg/energy'] + [f'MCMC/chain_{idx}/reg/energy' for idx in range(no_chains)] + \
-                ['VI/train/max_updates/' + parameter for parameter in ['mu', 'log_var', 'u']]
+                ['VI/train/max_updates/' + parameter for parameter in ['mu', 'log_var', 'log_u']]
 
         # metrics
         modes = ['train', 'test']
@@ -226,9 +226,10 @@ class ConfigParser:
     def init_optimizer_q_v(self, var_params_q_v):
         cfg_optimizer_q_v = self['optimizer_q_v']['args']
 
-        return torch.optim.Adam([{'params': [var_params_q_v['mu']], 'lr': cfg_optimizer_q_v['lr_mu']},
-                                 {'params': [var_params_q_v['log_var']], 'lr': cfg_optimizer_q_v['lr_log_var']},
-                                 {'params': [var_params_q_v['u']], 'lr': cfg_optimizer_q_v['lr_u']}])
+        return Adam([{'params': [var_params_q_v['mu']], 'lr': cfg_optimizer_q_v['lr_mu']},
+                     {'params': [var_params_q_v['log_var']], 'lr': cfg_optimizer_q_v['lr_log_var']},
+                     {'params': [var_params_q_v['log_u']], 'lr': cfg_optimizer_q_v['lr_log_u']}],
+                     lr_decay=cfg_optimizer_q_v['lr_decay'])
 
     def init_optimizer_reg(self, reg_loss):
         if self['optimizer_reg']['type'] != 'Adam':

utils/sampler.py
@@ -8,9 +8,11 @@ def sample_q_v(var_params_q_v, no_samples=1):
 
     mu_v = var_params_q_v['mu']
     log_var_v = var_params_q_v['log_var']
-    u_v = var_params_q_v['u']
+    log_u_v = var_params_q_v['log_u']
 
     sigma_v = torch.exp(0.5 * log_var_v)
+    u_v = torch.exp(log_u_v)
+    
     eps = torch.randn_like(sigma_v)
     x = torch.randn(1, device=u_v.device)
 